ff3d466d5f2ede3b4012178ab752aedf8b31461a
Jorge Cano <jacinaustin@gmail.com>
2017-12-08 18:51:08 -0600

    app.js changes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//original code
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
